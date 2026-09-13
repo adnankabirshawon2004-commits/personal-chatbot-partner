17e48a9531ed8a14e41c5b5fe95b66a9805bb10e
adnankabirshawon2004-commits <adnankabirshawon2004@gmail.com>
2026-09-13 21:28:48 +0600

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://bnagfjhghlncqqnenejn.supabase.co";
+
+const SUPABASE_KEY =
+  "sb_publishable_CEOgfj8n1K6CeIqizGVhTw_8oPlWuKk";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
